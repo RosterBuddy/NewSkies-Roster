84dfb1e2eb29da4279eb28d0b56fb4ff09e16de4
mrbreenhd <eliteracerYT@gmail.com>
2021-09-05 21:05:18 +0100

    Login redirects to roster page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = 'roster';
 
     /**
      * Create a new controller instance.
