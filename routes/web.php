b2f0c10eaef1f2b2637658492b74a2264a1822d0
mrbreenhd <eliteracerYT@gmail.com>
2021-09-05 21:05:08 +0100

    forces user to login route

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('roster', 'RostersController');
 
