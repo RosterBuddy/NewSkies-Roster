5a9717353e78bbfd9c009019dd9f07b0b3996247
mrbreenhd <eliteracerYT@gmail.com>
2021-09-06 21:08:48 +0100

    groups isLoggedIn function for roster and profile

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,15 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::resource('roster', 'RostersController');
 
-Route::resource('profile', 'ProfileController');
+Route::group(['middleware' => ['isLoggedIn']], function (){
 
+    Route::resource('roster', 'RostersController');
+    Route::resource('profile', 'ProfileController');
+
+});
+
+//User must be admin to access this.
 Route::group(['middleware' => ['admin']], function () {
     Route::resource('admin', 'AdminController');
     //Creates new user
@@ -29,4 +34,5 @@ Route::group(['middleware' => ['admin']], function () {
     //User Roster Updates
     Route::get('admin/user/roster', 'AdminController@user_calendar')->name('admin.show_user');
     Route::get('admin/user/roster/{id}', 'AdminController@show_user_calendar')->name('admin.show_user_calendar');
+    Route::post('admin/user/roster/{id}', 'AdminController@edit_user_calendar')->name('admin.edit_user_calendar');
 });
