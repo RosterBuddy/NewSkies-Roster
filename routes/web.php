ec2786c0a9a7779b77ffafd5c29710f34811da2f
mrbreenhd <eliteracerYT@gmail.com>
2021-09-12 22:09:53 +0100

    Reorganises routes file

--- routes/web.php
+++ routes/web.php
@@ -31,24 +31,25 @@ Route::group(['middleware' => ['admin']], function () {
     //Create Block Roster
     Route::get('admin/block', 'AdminController@block')->name('admin.block');
     Route::post('admin/create/block/', 'AdminController@create_block')->name('admin.create_block');
-
-
-    Route::resource('admin', 'AdminController');
-    
+   
     //Creates new user
     Route::get('admin/new/user', 'AdminController@makeuser')->name('admin.newuser');
     Route::post('admin/create/user', 'AdminController@createuser')->name('admin.create.user');
+
     //Update User Route
     //This route needs to be changed to admin/user and remove update from the middle
     Route::get('admin/update/user', 'AdminController@select_user_profile')->name('admin.select_user_profile');
     Route::get('admin/update/user/{admin}', 'AdminController@view_user_profile')->name('admin.view_user_profile');
     Route::patch('admin/update/user/{admin}/update', 'AdminController@update_user_profile')->name('admin.update_user_profile');
+
     //User Roster Updates
     Route::get('admin/user/roster', 'AdminController@user_calendar')->name('admin.show_user');
     Route::get('admin/user/roster/{admin}', 'AdminController@show_user_calendar')->name('admin.show_user_calendar');
     Route::get('admin/user/roster/{admin}/edit', 'AdminController@edit_user_calendar')->name('admin.edit_user_calendar');
     Route::patch('admin/user/roster/{admin}/update', 'AdminController@update_user_calendar')->name('admin.update_user_calendar');
 
+    Route::resource('admin', 'AdminController');
+    
     //Teams Routes
     Route::resource('teams', 'TeamsController');
 
