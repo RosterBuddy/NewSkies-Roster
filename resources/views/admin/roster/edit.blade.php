adcaa405bf4763c6452abb34fa21ca759007e967
mrbreenhd <eliteracerYT@gmail.com>
2021-09-12 22:08:00 +0100

    Basic layout

--- resources/views/admin/roster/edit.blade.php
+++ resources/views/admin/roster/edit.blade.php
@@ -1,30 +1,30 @@
 @extends('layouts.admin')
 @section('content')
 
-<form action="{{ route('admin.update_user_calendar', $roster->id) }}" method="post">
+<center>
+  <form action="{{ route('admin.update_user_calendar', $roster->id) }}" method="post">
     @csrf
     @method('PATCH')
     Agent Name: <input type="text" disabled value="{{$roster->user->name}}">
-    <br />
-    
-
-
-    <br /><br />
-    Description:
-    <br />
-    <textarea name="description"></textarea>
-    <br /><br />
-    Shift Start time:
-    <br />
-    <input type="datetime-local" value="{{$starttime}}" name="shift_start" class="date" />
-    <br /><br />
-    Shift End time:
-    <br />
-    <input type="datetime-local" value="{{$endtime}}" name="shift_end" class="date" />
-    <br /><br />
-    <input type="submit" value="Save" />
+    <br/>
+    <br/>
+    <br/>
+      Description:
+    <br/>
+      <input value=""  name="description">
+    <br/>
+    <br/>
+      Select Shift Time
+    <br/>
+    <select name="shift" id="shift">
+      <option value="early">Early</option>
+      <option value="late">Late</option>
+    </select>
+    <br/>
+    <br/>
+      <input type="submit" value="Save" />
   </form>
-
+</center>
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="https://code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
 @endsection 
