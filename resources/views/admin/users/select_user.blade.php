8ffc27a582a14b7582729ed937f40748fa81b823
mrbreenhd <eliteracerYT@gmail.com>
2021-09-12 22:40:22 +0100

    centers table

--- resources/views/admin/users/select_user.blade.php
+++ resources/views/admin/users/select_user.blade.php
@@ -1,21 +1,19 @@
 @extends('layouts.admin')
 @section('content')
-
-<table border="1">
-    <tr>
-        <th>Name</th>
-        <th>E-Mail</th>
-        <th>Roster</th>
-    </tr>
-    
-    @foreach ($users as $user)
-    <tr>
-        <th>{{$user->name}}</th>
-        <th>{{$user->email}}</th>
-        <th><a href="{{ route('admin.view_user_profile',$user->id)}}">Edit User</a></th>
-    </tr>
-    @endforeach
-    
-</table>
-
+<center>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>E-Mail</th>
+            <th>Roster</th>
+        </tr>
+        @foreach ($users as $user)
+        <tr>
+            <th>{{$user->name}}</th>
+            <th>{{$user->email}}</th>
+            <th><a href="{{ route('admin.view_user_profile',$user->id)}}">Edit User</a></th>
+        </tr>
+        @endforeach
+    </table>
+</center>
 @endsection
