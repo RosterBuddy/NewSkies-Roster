b368855d3bc9c0044b943ce973988942bca7859e
mrbreenhd <eliteracerYT@gmail.com>
2021-09-12 22:08:12 +0100

    Centers table

--- resources/views/admin/roster/list.blade.php
+++ resources/views/admin/roster/list.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.admin')
 @section('content')
 
-<table border="1">
-    <tr>
-        <th>Name</th>
-        <th>E-Mail</th>
-        <th>Roster</th>
-    </tr>
-    
-    @foreach ($users as $user)
-    <tr>
-        <th>{{$user->name}}</th>
-        <th>{{$user->email}}</th>
-        <th><a href="{{ route('admin.show_user_calendar',$user->id)}}">Show Roster</a></th>
-    </tr>
-    @endforeach
-    
-</table>
+<center>
+    <table border="1">
+        <tr>
+            <th>Name</th>
+            <th>E-Mail</th>
+            <th>Roster</th>
+        </tr>
+        @foreach ($users as $user)
+        <tr>
+            <th>{{$user->name}}</th>
+            <th>{{$user->email}}</th>
+            <th><a href="{{ route('admin.show_user_calendar',$user->id)}}">Show Roster</a></th>
+        </tr>
+        @endforeach
+    </table>
+</center>
 
 @endsection
