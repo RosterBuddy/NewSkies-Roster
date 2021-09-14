71270326482de434c2519be14c96fdd52853baf8
mrbreenhd <eliteracerYT@gmail.com>
2021-09-14 17:58:27 +0100

    changes default value to 1

--- database/migrations/2021_09_08_205317_add_team_to_users.php
+++ database/migrations/2021_09_08_205317_add_team_to_users.php
@@ -14,7 +14,7 @@ class AddTeamToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('team_id')->after('password')->default(5);
+            $table->integer('team_id')->after('password')->default(1);
         });
     }
 
