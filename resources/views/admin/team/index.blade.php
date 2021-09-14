93490e1bc8be515c3dbe8fafd0d4b8201b9ee01a
mrbreenhd <eliteracerYT@gmail.com>
2021-09-14 18:46:56 +0100

    displays count of users

--- resources/views/admin/team/index.blade.php
+++ resources/views/admin/team/index.blade.php
@@ -1,17 +1,38 @@
 @extends('layouts.admin')
 @section('content')
+<style>
+    .myTable {
+        max-width: 19%;
+    }
+</style>
 <center>
-<h1>Not Completed</h1>
-    <table border="1">
-        <tr>
-            <th>Team Name</th>
-        </tr>    
-        @foreach ($teams as $team)
-        <tr>
-            <th>{{$team->team_name}}</th>
-            <th><a href="#"></a></th>
-        </tr>
+<div class="myTable">
+    <h1>Not Completed</h1>
+        <table  class="table table-striped">
+            <tr>
+                <th>Team Name</th>
+                <th>No. Members</th>
+                <th></th>
+            </tr>    
+            @foreach ($teams as $team)
+            <tr>
+                <th style="text-align:center">{{$team->team_name}}</th>
+                <th style="text-align:center">@if($team->id == 1)
+                        {{$unassigned}}
+                    @elseif($team->id == 2)
+                        {{$coms}}
+                    @elseif($team->id == 3)
+                        {{$disruptions}}
+                    @elseif($team->id == 4)
+                        {{$systems}}
+                    @endif
+                </th>
+                <th style="text-align:center"><a href="{{route('teams.show', $team->id)}}">View Team</a></th>
+            </tr>
         @endforeach        
     </table>
+</div>
 </center>
 @endsection
+
+
