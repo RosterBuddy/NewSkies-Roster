a7a5d12880506e5b6ca5d65b794f832ac781b752
mrbreenhd <eliteracerYT@gmail.com>
2021-10-09 18:31:10 +0100

    Create TimingController.php

---
+++ app/Http/Controllers/TimingController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Timing;
+
+class TimingController extends Controller
+{
+    public function index(){
+        $timings = Timing::all();
+        return view('admin.team.timings.index', compact('timings'));
+    }
+
+    public function edit($id){
+        $timing = Timing::find($id);
+        return view('admin.team.timings.edit', compact('timing'));
+    }
+
+    public function create(){
+        Timing::create([
+            'name' => $request['name'],
+            'shift_start' => $request["shift_start"],
+            'shift_end' => $request["shift_end"],
+        ]);
+        toastr()->success('Time Created Successfully!');
+        return redirect()->route('timing.index');
+    }
+
+    public function update(Request $request, $id){
+        $timing = timing::find($id);
+        $timing->shift_start = $request->shift_start;
+        $timing->shift_end = $request->shift_end;
+        $timing->save();
+
+        toastr()->success('Time Updated Successfully!');
+        return redirect()->route('timing.index');
+    }    
+}
