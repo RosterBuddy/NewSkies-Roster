7be897fdd5471022c4a2b6cfcc725c55248b98c9
mrbreenhd <eliteracerYT@gmail.com>
2021-10-09 18:31:36 +0100

    timing table

---
+++ resources/views/admin/team/timings/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.admin')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card text-center">
+                <table class="table table-striped">
+                    <tr>
+                        <th>Name</th>
+                        <th>Start Time</th>
+                        <th>Finish Time</th>
+                        <th><a href="">Create Time</a></th>
+                    </tr>    
+                    @foreach($timings as $timing)
+                    <tr>
+                        <th style="text-align:center">{{$timing->name}}</th>
+                        <th style="text-align:center">{{\Carbon\Carbon::createFromFormat('H:i:s', $timing->shift_start)->format('H:i')}}</th>
+                        <th style="text-align:center">{{\Carbon\Carbon::createFromFormat('H:i:s', $timing->shift_end)->format('H:i')}}</th>
+                        <th style="text-align:center"><a href="{{route('timing.edit', $timing->id)}}">Edit Timing</a></th>
+                    </tr>
+                    @endforeach
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
