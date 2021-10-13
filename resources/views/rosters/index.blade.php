7987eaa553bda1ba45682fcd7823e4da3eeb03cd
mrbreenhd <eliteracerYT@gmail.com>
2021-10-14 00:11:56 +0100

    changes check to only show if isn't off

--- resources/views/rosters/index.blade.php
+++ resources/views/rosters/index.blade.php
@@ -51,7 +51,7 @@ html, body {
 
             events : [
                 @foreach($rosters as $roster)
-                    @if($roster->description == "work" || $roster->description == null && $roster->annual_leave != 1)
+                    @if($roster->description != "off" && $roster->annual_leave != 1)
                         {
                             title : '{{$roster->user->name}}',
                             @if($roster->user->isAdmin == 1 && $roster->user->team_id == 2)
