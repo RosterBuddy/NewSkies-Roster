37346e266867767c84fc211650c8bf3b8724de23
mrbreenhd <eliteracerYT@gmail.com>
2021-10-13 02:14:22 +0100

    adds new user type

--- app/Timing.php
+++ app/Timing.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Timing extends Model
 {
     protected $fillable = [
-        'name', 'shift_start', 'shift_end'
+        'name', 'shift_start', 'shift_end', 'user_type'
     ];
 }
