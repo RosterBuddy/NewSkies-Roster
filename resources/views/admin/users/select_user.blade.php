f38b7dccf1d49031c09905408380a6ed4f50dc05
mrbreenhd <eliteracerYT@gmail.com>
2021-10-16 23:07:40 +0100

    Only shows active users

--- resources/views/admin/users/select_user.blade.php
+++ resources/views/admin/users/select_user.blade.php
@@ -1,23 +1,30 @@
 @extends('layouts.admin')
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+<div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <table class="table table-striped">
-                <tr>
-                    <th>Name</th>
-                    <th>E-Mail</th>
-                    <th>Roster</th>
-                </tr>
-                @foreach ($users as $user)
-                <tr>
-                    <th>{{$user->name}}</th>
-                    <th>{{$user->email}}</th>
-                    <th><a href="{{ route('admin.view_user_profile',$user->id)}}">Edit User</a></th>
-                </tr>
-                @endforeach
-            </table>
+            <div class="card text-center">
+                <div class="card-header">Active Users</div>
+                    <div class="card-body">
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Name</th>
+                                <th>E-Mail</th>
+                                <th>Roster</th>
+                            </tr>
+                            @foreach ($users as $user)
+                                @if($user->isInactive == 0)
+                                <tr>
+                                    <th>{{$user->name}}</th>
+                                    <th>{{$user->email}}</th>
+                                    <th><a href="{{ route('admin.view_user_profile',$user->id)}}">Edit User</a></th>
+                                </tr>
+                                @endif
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
