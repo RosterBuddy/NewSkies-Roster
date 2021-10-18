b3e5be5f2fd02a06ba412adfa02950610653ab08
mrbreenhd <eliteracerYT@gmail.com>
2021-10-18 15:32:25 +0100

    Adds email logic and DB update

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -6,20 +6,37 @@ use Illuminate\Http\Request;
 use App\Roster;
 use Carbon\Carbon;
 use App\User;
-use Email;
+use Mail;
 
 class EmailController extends Controller
 {
     public function lastedited($id) {
-        $date  = Carbon::now()->subMinutes(10);
-        $date->setTimezone('Europe/Dublin');
-
         $user = User::find($id);
-
+        $date  = Carbon::now()->subHours(3);
 
         $rosteredits = Roster::where('updated_at', '>=', $date)->get();
 
-        return view('admin.email.lastedited', compact('rosteredits', 'user'));
+       foreach($rosteredits as $rosteredit){
+          if($rosteredit->updated_at != $rosteredit->last_edited){
+              $roster = Roster::find($rosteredit->id);
+              $roster->updated_at = Carbon::now();
+              $roster->last_edited = $roster->updated_at;
+              $roster->save();
+          }
+       }
 
+        Mail::send('admin.email.lastedited', [
+            'rosteredits' => $rosteredits,
+            'user' => $user,
+        ], function ($mail) use($id) {    
+            $user = User::find($id);
+            $mail->from('no-reply@rosterbuddy.org', "Newskies Managment");
+            $mail->to($user->email)->subject("An Update To Your Roster");
+        });
+
+        toastr()->success('Agent advised of changes in their roster.', 'Complete');
+        return back()->withInput();
+
+        //return view('admin.email.lastedited', compact('rosteredits', 'user'));
     }
 }
