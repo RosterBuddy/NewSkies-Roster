6b6d601c334829176681514e6088891e2aff80a6
mrbreenhd <eliteracerYT@gmail.com>
2021-11-11 21:11:06 +0000

    changes fillable

--- app/Roster.php
+++ app/Roster.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Roster extends Model
 {
-    protected $fillable = ['name', 'user_id', 'description', 'annual_leave', 'shift_start', 'shift_end'];
+    protected $fillable = ['name', 'user_id', 'description', 'day_off', 'shift_start', 'shift_end'];
 
     public function User() {
         return $this->belongsTo(User::class);
