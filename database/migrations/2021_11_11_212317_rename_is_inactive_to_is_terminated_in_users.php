67bfa79cd56576843597ae113aacda2424a4390d
mrbreenhd <eliteracerYT@gmail.com>
2021-11-11 21:26:25 +0000

    renames column to isTermianted #19

---
+++ database/migrations/2021_11_11_212317_rename_is_inactive_to_is_terminated_in_users.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RenameIsInactiveToIsTerminatedInUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('isInactive', 'isTermianted');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('is_terminated_in_users', function (Blueprint $table) {
+            //
+        });
+    }
+}
