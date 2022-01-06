4a3372b07b78c4ffa76358087811540aaf9b1064
mrbreenhd <eliteracerYT@gmail.com>
2022-01-06 01:12:49 +0000

    Correct filter REF: #25

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -67,7 +67,7 @@ html, body {
             events : [
                 @foreach($rosters as $roster)
                     @if($roster->user_id == $id)
-                        @if($roster->description == "work" || $roster->description == null && $roster->annual_leave == 0)
+                        @if($roster->description != "off" && $roster->day_off == 0)
                             {
                                 title : '{{$roster->user->name}}',
                                 backgroundColor : '{{$roster->user->color}}',
