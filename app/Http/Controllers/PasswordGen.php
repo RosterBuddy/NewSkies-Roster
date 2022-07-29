ea06487c82b6bc060542cccda677c2cb879ac783
mrbreenhd <eliteracerYT@gmail.com>
2022-07-29 18:31:13 +0100

    adds password generator

---
+++ app/Http/Controllers/PasswordGen.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PasswordGen extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $pass = array(); 
+        $alphaLength = strlen($alphabet) - 1;
+        for ($i = 0; $i < 8; $i++) {
+            $n = rand(0, $alphaLength);
+            $pass[] = $alphabet[$n];
+        }
+        return implode($pass);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return redirect('login');
 });
 
+Route::get('passwordgen', 'PasswordGen@index')->name('password');
+
 Auth::routes(['verify' => true, 'register' => false]);
 
 Route::get('weather', 'MetarController@metar')->name('test.weather');
