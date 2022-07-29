78bdc238582d22eef46dad42193eaadeca4b0cb6
mrbreenhd <eliteracerYT@gmail.com>
2022-07-29 20:23:48 +0100

    opsk

--- resources/views/password.blade.php
+++ resources/views/password.blade.php
@@ -1,4 +1,4 @@
 @extends('layouts.app')
 @section('content')
-<H2>New Password: <span style="color:red;">{{$result}}</span></H2>
+<H2>New Password: <span style="color:red;">{{$str}}</span></H2>
 @endsection
