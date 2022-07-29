7b9137e10b62f4bedca9ec9f03e607f6d77f4e43
mrbreenhd <eliteracerYT@gmail.com>
2022-07-29 20:31:30 +0100

    Line break

--- resources/views/password.blade.php
+++ resources/views/password.blade.php
@@ -2,5 +2,6 @@
 @section('content')
 <button type="button" onClick="window.location.reload();">Generate New Password</button>
 <br>
+<br>
 <H2>New Password: <span style="color:red;">{{$str}}</span></H2>
 @endsection
