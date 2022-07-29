7ac7793a5d6fa8f23d2c13cc84d1d0d5bab7aaf3
mrbreenhd <eliteracerYT@gmail.com>
2022-07-29 20:28:30 +0100

    Adds button to refresh

--- resources/views/password.blade.php
+++ resources/views/password.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.app')
 @section('content')
+<button type="button" onClick="window.location.reload();">Generate New Password</button>
+<br>
 <H2>New Password: <span style="color:red;">{{$str}}</span></H2>
 @endsection
