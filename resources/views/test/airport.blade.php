e0bd8d060a2c2e17458421b7047e069316c83a6e
mrbreenhd <eliteracerYT@gmail.com>
2022-09-09 19:24:47 +0100

    added foreach

--- resources/views/test/airport.blade.php
+++ resources/views/test/airport.blade.php
@@ -1,3 +1,5 @@
-Airport Name: {{$airport->name}}<br>
-Airport Code: {{$airport->iata}} // {{$airport->icao}}<br>
-Timezone: {{$airport->timezone}}<br>
+@foreach($airport as $apt)
+Airport Name: {{$apt->name}}<br>
+Airport Code: {{$apt->iata}} // {{$apt->icao}}<br>
+Timezone: {{$apt->timezone}}<br>
+@endforeach
