8428d1a9a4ba7d216c8cc03f6db47d11ec9ddff1
mrbreenhd <eliteracerYT@gmail.com>
2022-09-09 20:50:34 +0100

    maybe this works

--- resources/views/test/airport.blade.php
+++ resources/views/test/airport.blade.php
@@ -1,29 +1,3 @@
-@foreach($airport as $apt)
-    Airport Name: {{$apt->name}}<br>
-    Airport Code: {{$apt->iata}} // {{$apt->icao}}<br>
-    Country: {{$apt->country}}<br>
-    Current Time: {{\Carbon\Carbon::now()->timezone($apt->db_timezone)->format('H:i')}} local time<br>
-
-    <script>
-        L.marker([{{$apt->latitude}}, {{$apt->longitude}}]).addTo(mymap);
-    </script>
-@endforeach
-
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
-
-<style>
-    #mapid { 
-        height: 100%;
-        width: 100%;
-    }
-</style>
-
-<div id="mapid"></div>
-
-
 <script>
 var weatherLayer = new L.LayerGroup(); // NOTE: Layer is created here!
     var apiData = {};
@@ -190,3 +164,29 @@ var mymap = L.map('mapid').setView([47.4177873, 9.0855997], 4);
         layer.bindPopup(popupContent);
     }
 </script>
+
+
+@foreach($airport as $apt)
+    Airport Name: {{$apt->name}}<br>
+    Airport Code: {{$apt->iata}} // {{$apt->icao}}<br>
+    Country: {{$apt->country}}<br>
+    Current Time: {{\Carbon\Carbon::now()->timezone($apt->db_timezone)->format('H:i')}} local time<br>
+
+    <script>
+        L.marker([{{$apt->latitude}}, {{$apt->longitude}}]).addTo(mymap);
+    </script>
+@endforeach
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+
+<style>
+    #mapid { 
+        height: 100%;
+        width: 100%;
+    }
+</style>
+
+<div id="mapid"></div>
