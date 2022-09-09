f49532fd3cd3b35f92c48ee352596d45ee5b87a9
mrbreenhd <eliteracerYT@gmail.com>
2022-09-09 20:45:53 +0100

    adds map

--- resources/views/test/airport.blade.php
+++ resources/views/test/airport.blade.php
@@ -2,28 +2,14 @@
     Airport Name: {{$apt->name}}<br>
     Airport Code: {{$apt->iata}} // {{$apt->icao}}<br>
     Country: {{$apt->country}}<br>
-    Current Time: {{\Carbon\Carbon::now()->timezone($apt->db_timezone)->format('H:i')}}<br>
+    Current Time: {{\Carbon\Carbon::now()->timezone($apt->db_timezone)->format('H:i')}} local time<br>
+@endforeach
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 
-<script src="{{asset('js/leaflet.rotatedMarker.js')}}"></script>
 <style>
     #mapid { 
         height: 100%;
@@ -32,6 +18,7 @@
 </style>
 
 <div id="mapid"></div>
+
 
 <script>
 var weatherLayer = new L.LayerGroup(); // NOTE: Layer is created here!
@@ -198,5 +185,4 @@ var mymap = L.map('mapid').setView([47.4177873, 9.0855997], 4);
 
         layer.bindPopup(popupContent);
     }
-</script> -->
-@endforeach
+</script>
