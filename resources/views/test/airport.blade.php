bc1a67fa0a5d9a59265aa78a6b00be4b25e23773
mrbreenhd <eliteracerYT@gmail.com>
2022-09-25 07:48:50 +0100

    Tidied up

--- resources/views/test/airport.blade.php
+++ resources/views/test/airport.blade.php
@@ -1,68 +1,65 @@
 <body style="font-size: 40px;">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossorigin=""/>
-<script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+    @foreach($airport as $apt)
+        Airport Name: {{$apt->name}}<br>
+        Airport Code: {{$apt->iata}} // {{$apt->icao}}<br>
+        Country: {{$apt->country}}<br>
+        Current Time: {{\Carbon\Carbon::now()->timezone($apt->db_timezone)->format('H:i')}} local time<br>
 
+        <style>
+            #mapid { 
+                height: 71%;
+                width: 100%;
+            }
+        </style>
+        <br>
+        <div id="mapid"></div>
 
+        <script>
+        var mymap = L.map('mapid').setView([47.4177873, 9.0855997], 4);
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox/dark-v10',
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: 'pk.eyJ1IjoiMTMwNjYzOSIsImEiOiJjaXVheHF1emUwMDBnMnZ1dnF3NXlkcHJsIn0.EA9FU7f3QqXtirYubf6hvg'
+            }).addTo(mymap);
 
-@foreach($airport as $apt)
-    Airport Name: {{$apt->name}}<br>
-    Airport Code: {{$apt->iata}} // {{$apt->icao}}<br>
-    Country: {{$apt->country}}<br>
-    Current Time: {{\Carbon\Carbon::now()->timezone($apt->db_timezone)->format('H:i')}} local time<br>
+            L.marker([{{$apt->latitude}}, {{$apt->longitude}}]).addTo(mymap);
 
-    <style>
-    #mapid { 
-        height: 71%;
-        width: 100%;
-    }
-</style>
-<br>
-<div id="mapid"></div>
+            function polystyle(feature) {
+                return {
+                    fillColor: 'white',
+                    weight: 0.5,
+                    opacity: 0.3,
+                    color: 'white',  //Outline color
+                    fillOpacity: 0
+                };
+            }
 
-<script>
-var mymap = L.map('mapid').setView([47.4177873, 9.0855997], 4);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox/dark-v10',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoiMTMwNjYzOSIsImEiOiJjaXVheHF1emUwMDBnMnZ1dnF3NXlkcHJsIn0.EA9FU7f3QqXtirYubf6hvg'
-    }).addTo(mymap);
+            $.ajax({
+                type: "GET",
+                url: "https://raw.githubusercontent.com/vatsimnetwork/vatspy-data-project/master/Boundaries.geojson",
+                dataType: 'json',
+                success: function (response) {
 
-    L.marker([{{$apt->latitude}}, {{$apt->longitude}}]).addTo(mymap);
+                    geojsonLayer = L.geoJson(response, {
+                        style: polystyle,
+                        onEachFeature: function(feature, layer){
+                        layer.bindPopup(feature.properties.id); 
+                    }}).addTo(mymap);
+                }
+            });
 
-    function polystyle(feature) {
-        return {
-            fillColor: 'white',
-            weight: 0.5,
-            opacity: 0.3,
-            color: 'white',  //Outline color
-            fillOpacity: 0
-        };
-    }
+            function forEachFeature(feature, layer) {
+                var popupContent = "<p>" + feature.properties +"</p>";
 
-    $.ajax({
-        type: "GET",
-        url: "https://raw.githubusercontent.com/vatsimnetwork/vatspy-data-project/master/Boundaries.geojson",
-        dataType: 'json',
-        success: function (response) {
-
-            geojsonLayer = L.geoJson(response, {
-                style: polystyle,
-                onEachFeature: function(feature, layer){
-                layer.bindPopup(feature.properties.id); 
-            }}).addTo(mymap);
-        }
-    });
-
-    function forEachFeature(feature, layer) {
-        var popupContent = "<p>" + feature.properties +"</p>";
-
-        layer.bindPopup(popupContent);
-    }
-</script>
-@endforeach
+                layer.bindPopup(popupContent);
+            }
+        </script>
+    @endforeach
 </body>
