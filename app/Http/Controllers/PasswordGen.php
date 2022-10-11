8830bae4c428584cf84d693c8e2e07a1671a550d
mrbreenhd <eliteracerYT@gmail.com>
2022-10-11 13:03:45 +0100

    generate 500

--- app/Http/Controllers/PasswordGen.php
+++ app/Http/Controllers/PasswordGen.php
@@ -12,13 +12,23 @@ class PasswordGen extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index($id){
-        $chars =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.'0123456789-=!@#$%&';
+        // $chars =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.'0123456789-=!@#$%&';
       
-        $str = '';
-        $max = strlen($chars) - 1;
+        // $str = '';
+        // $max = strlen($chars) - 1;
       
-        for ($i=0; $i < $id; $i++)
-          $str .= $chars[random_int(0, $max)];
+        // for ($i=0; $i < $id; $i++)
+        //   $str .= $chars[random_int(0, $max)];
+
+      $str = '';
+        function password_generate($chars) 
+        {
+          $data = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=!@#$%&';
+          return substr(str_shuffle($data), 0, $chars);
+        }
+        for($t=0; $t < 500; $t++;){
+          $str = password_generate(12)."\n";
+        }
       
         return view('password', compact('str'));
     }
