010c1fdfe0a17bf5aba862add0323841676af23b
mrbreenhd <eliteracerYT@gmail.com>
2022-10-11 18:57:03 +0100

    returned to old way

--- app/Http/Controllers/PasswordGen.php
+++ app/Http/Controllers/PasswordGen.php
@@ -12,23 +12,13 @@ class PasswordGen extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index($id){
-        // $chars =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.'0123456789-=!@#$%&';
+        $chars =  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.'0123456789-=!@#$%&';
       
-        // $str = '';
-        // $max = strlen($chars) - 1;
-      
-        // for ($i=0; $i < $id; $i++)
-        //   $str .= $chars[random_int(0, $max)];
-
         $str = '';
-        function password_generate($chars) {
-          $data = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-=!@#$%&';
-          return substr(str_shuffle($data), 0, $chars);
-        }
-
-        for($t=0; $t < 500; $t++){
-          echo password_generate($id)."\n";
-        }
+        $max = strlen($chars) - 1;
+      
+        for ($i=0; $i < $id; $i++)
+          $str .= $chars[random_int(0, $max)];
       
         return view('password', compact('str'));
     }
