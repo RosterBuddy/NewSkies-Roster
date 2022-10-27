221508f7b3ef0303f080e22635d9c8c3c99baf5a
mrbreenhd <eliteracerYT@gmail.com>
2022-10-27 08:16:39 +0100

    Basic table

--- resources/views/test/youtube.blade.php
+++ resources/views/test/youtube.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 @section('content')
-
-@foreach($comments as $comment)
-    {{$comment}}
-@endforeach
-
-
+    <table>
+        <tr>
+            <th>Video ID</th>
+            <th>Comment</th>
+        </tr>
+        @foreach($comments as $comment)
+        <tr>
+            <td>{{$comment["snippet"]["videoId"]}}</td>
+            <td>{{$comment["snippet"]["topLevelComment"]["snippet"]["textOriginal"]}}</td>
+        </tr>
+        @endforeach
+    </table>
 @endsection
