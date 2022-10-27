9dd1eb050adb5f8b959740a5801010bf3e221fc3
mrbreenhd <eliteracerYT@gmail.com>
2022-10-27 13:08:26 +0100

    gets items

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -37,7 +37,7 @@ class HomeController extends Controller
 
     public function videos($id)
     {
-        $playlist = Youtube::getPlaylistById($id);
+        $playlist = Youtube::getPlaylistItemsByPlaylistId($id);
 
         dd($playlist);
     }
