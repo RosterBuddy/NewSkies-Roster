b1a55b91e8d47a75ccea71cf1b515c01cafd3b08
mrbreenhd <eliteracerYT@gmail.com>
2022-10-27 07:25:26 +0100

    123

--- resources/views/test/youtube.blade.php
+++ resources/views/test/youtube.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
-@foreach($commentThreads as $comments)
-    {{$comments}}
+@foreach($commentThreads as $comments => $key)
+    {{$key}}
 @endforeach
 
 @endsection
